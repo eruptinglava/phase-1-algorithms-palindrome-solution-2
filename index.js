02fc37a56cfa63b9e04dce3dfaf16375738b7e3a
Lava Ahmed <lavaahmed915@gmail.com>
2024-05-14 19:10:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i;
+    firstPart = word[i];
+    secondPart = word[j];
+    if (firstPart!== secondPart) {
+      return false;
+    }
+    return true;
+  }
 }
 
 /* 
   Add your pseudocode here
+  split the word into 2 parts and itreate throw the first part 
+  and the second part if they are the same
+  return true if not flase
 */
 
 /*
   Add written explanation of your solution here
+  first we divide the words into 2 parts
+  the itreate throw both parts but for the second
+  part we start with last letter then each part
+  saved in a variable
+  if the first part is not the same as the second part
+  return false
+  else return true
+
+
 */
 
 // You can run `node index.js` to view these console logs
